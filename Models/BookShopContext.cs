7f47c1e77177897ad6103da96cebf9e04038d9e4
AmirhosseinSoltani <83413873+AmirhosseinSoltani@users.noreply.github.com>
2021-04-29 20:06:17 +0430

    Add files via upload

---
+++ Models/BookShopContext.cs
@@ -0,0 +1,43 @@
+﻿using BookShop.Mapping;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookShop.Models;
+
+namespace BookShop.Models
+{
+    public class BookShopContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(local);Database=BookShopDB;Trusted_Connection=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new Author_BookMap());
+            modelBuilder.ApplyConfiguration(new CustomerMap());
+            modelBuilder.ApplyConfiguration(new Order_BookMap());
+            modelBuilder.ApplyConfiguration(new Book_TranslatorMap());
+            modelBuilder.ApplyConfiguration(new Book_CategoryMap());
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Provice> Provices { get; set; }
+        public DbSet<Author_Book> Author_Books { get; set; }
+        public DbSet<Order_Book> Order_Books { get; set; }
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<Discount> Discounts { get; set; }
+        public DbSet<OrderStatus> OrderStatuses { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Translator> Translator { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<Book_Category> Book_Categories { get; set; }
+    }
+}
